05f531b6d6dc802d864dae350525434566303fb3
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2014-01-06 22:11:50 +0000

    adding test cases for the different functions

--- businesshours.py
+++ businesshours.py
@@ -26,7 +26,7 @@ def main():
 
     return
 
-def periodize(hours, P = timedelta(days=7)):
+def periodize(events, P = timedelta(days=7)):
     """
     takes a list that represents a period of event and extend it with events
     e+P , e-P where P is the timedelta of the period. The Period defaults to
@@ -35,12 +35,21 @@ def periodize(hours, P = timedelta(days=7)):
     EXAMPLE
     =======
 
-    >>> hours = [(datetime(2000,1,15,9,0),'open'),(datetime(2000,1,15,23,0),'close')]
+    >>> hours = [(datetime(2000,1,15,9,0),'open')]
     >>> periodize(hours)
-    [(datetime(2000,1,15,9,0),'open'), (datetime(2000,1,15,23,0),'close')]
-
+    [(datetime.datetime(2000, 1, 8, 9, 0), 'open'), (datetime.datetime(2000, 1, 15, 9, 0), 'open'), (datetime.datetime(2000, 1, 22, 9, 0), 'open')]
 
     """
+    result = []
+    for event in events:
+        time_event_minus = event[0] - P
+        time_event_plus = event[0] + P
+        result.extend([(time_event_minus,event[1]),
+                        event,
+                        (time_event_plus,event[1])])
+
+    result.sort()
+    return result
 
 def transform(elem):
     """
@@ -87,6 +96,67 @@ def transform(elem):
     result = (date,state)
     return result
 
+def immediately_prior(H,d):
+    """
+    return the event in H that is immediately prior in time (or
+    simultanously occuring) to d.
+
+    EXAMPLE
+    =======
+
+    >>> H = [(datetime(2000, 1, 8, 9, 0), 'open'), (datetime(2000, 1, 15, 9, 0), 'open'), (datetime(2000, 1, 22, 9, 0), 'open')]
+    >>> d = datetime(2000,1,15,8,0)
+    >>> immediately_prior(H,d)
+    (datetime.datetime(2000,1,8,9,0), 'open')
+    """
+
+    return (datetime(1999,1,1,1,1),'open')
+
+def event_between(H, d1, d2):
+    """
+    return the events in H that are between the range [d1,d2[
+
+    EXMAPLE
+    =======
+
+    >>> H = [(datetime(2000, 1, 8, 9, 0), 'open'), (datetime(2000, 1, 15, 9, 0), 'open'), (datetime(2000, 1, 22, 9, 0), 'open')]
+    >>> d1 = datetime(2000,1,14,9,0)
+    >>> d2 = datetime(2000,1,16,9,0)
+    >>> event_between(H,d1,d2)
+    [datetime.datetime(2000,1,15,9,0)]
+    """
+    return []
+
+
+def opened_range(H,d1,d2):
+    """
+    return true if, for the given list of event H, there is a continuous
+    open period between d1 and d2.
+
+    EXAMPLE
+    =======
+
+    # opened inside the range.
+    >>> h = periodize([(datetime(2000,1,15,23,0), 'close'), (datetime(2000,1,16,5,0), 'open')])
+    >>> d1 = datetime(2000,1,16,9,0)
+    >>> d2 = datetime(2000,1,16,10,0)
+    >>> opened_range(h,d1,d2)
+    True
+
+    # closed inside the range.
+    >>> h = periodize([(datetime(2000,1,15,20,0), 'close'), (datetime(2000,1,16,5,0), 'open')])
+    >>> d1 = datetime(2000,1,15,21,0)
+    >>> d2 = datetime(2000,1,15,22,0)
+    >>> opened_range(h,d1,d2)
+    False
+    """
+
+    prior = immediately_prior(H,d1)
+    events_in = event_between(H, d1, d2)
+    close_events_in = [e for e in events_in if e[1] == 'close']
+
+    result = prior[1] == 'open' and len(close_events_in) == 0
+    return result
 
 if __name__ == '__main__':
     main()
